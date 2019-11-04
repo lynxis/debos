fc6be9f6fd0c577a79a806b0c9f1d6cb7fa08681
Alexander Couzens <lynxis@fe80.eu>
2019-11-04 07:10:37 +0100

    add logging

--- commands.go
+++ commands.go
@@ -122,12 +122,15 @@ func (cmd Command) Run(label string, cmdline ...string) error {
 	var options []string
 	switch cmd.ChrootMethod {
 	case CHROOT_METHOD_NONE:
+		log.Println("WARNING: LYNX: NONE")
 		options = cmdline
 	case CHROOT_METHOD_CHROOT:
+		log.Println("WARNING: LYNX: CHROOT")
 		options = append(options, "chroot")
 		options = append(options, cmd.Chroot)
 		options = append(options, cmdline...)
 	case CHROOT_METHOD_NSPAWN:
+		log.Println("WARNING: LYNX: NSPAWN")
 		options = append(options, "systemd-nspawn", "-q", "-D", cmd.Chroot)
 		for _, e := range cmd.extraEnv {
 			options = append(options, "--setenv", e)
